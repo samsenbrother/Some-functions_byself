27f326b4d41a4f5143c4cab539fbb9407b57f6ae
samsenbrother <css.qrs@gmail.com>
2014-12-11 09:26:09 +0800

    could copy all of the code except the fisrt number that can use directly now

--- usepdf_dir.R
+++ usepdf_dir.R
@@ -1,5 +1,6 @@
 #define a function for using sentences copyed directly from PDF
 #method:pdf_dir('paste the code')
 pdf_dir<-function(a){
+  a<-gsub('\n[1-9]','\n ',a)
   eval(parse(text=gsub(' ','',a)))
 }
