27272153d3123cb6abe1fbec824a55aea1db6dd3
samsenbrother <css.qrs@gmail.com>
2014-12-11 10:10:38 +0800

    pdf_dir('',paramater 2(default=T),if the secend paramter equals T,this function just allow to  evaluate the last sentence of the output from the function "parse()".So you can use it like this"pdf_dir('',F) to see all of the results

--- usepdf_dir.R
+++ usepdf_dir.R
@@ -1,6 +1,15 @@
 #define a function for using sentences copyed directly from PDF
 #method:pdf_dir('paste the code')
-pdf_dir<-function(a){
+pdf_dir<-function(a,all=T){
   a<-gsub('\n[1-9]','\n ',a)
+  if(all==T){
   eval(parse(text=gsub(' ','',a)))
+  }
+  else{
+    parse(text=gsub(' ','',a))->output
+    length=length(output)
+    for(i in 1:length){
+      print(eval(output[i]))
+    }
+  }
 }
