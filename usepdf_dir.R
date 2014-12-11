51a2c7fd55da481aeda6c9fc88101b3ce91b6627
samsenbrother <css.qrs@gmail.com>
2014-12-11 15:16:52 +0800

    But still could not solve the problem about the accident of the code

--- usepdf_dir.R
+++ usepdf_dir.R
@@ -1,7 +1,11 @@
 #define a function for using sentences copyed directly from PDF
 #method:pdf_dir('paste the code')
 pdf_dir<-function(a,all=T){
+  a<-gsub('¤','*',a)
+  a<-gsub('^> ','  ',a)
+  a<-gsub('<¡','<-',a)
   a<-gsub('\n[1-9]','\n ',a)
+  a<-gsub('^<<+.>$','<-',a)
   if(all==T){
   eval(parse(text=gsub(' ','',a)))
   }
@@ -9,7 +13,7 @@ pdf_dir<-function(a,all=T){
     parse(text=gsub(' ','',a))->output
     length=length(output)
     for(i in 1:length){
-      print(eval(output[i]))
+      eval(output[i])
     }
   }
 }
